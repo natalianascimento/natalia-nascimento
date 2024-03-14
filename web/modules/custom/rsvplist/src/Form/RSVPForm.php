03a57e461f79da8bd628ad13a82e05640a3ac69d
Natalia <natalia.nascc@gmail.com>
2024-03-13 23:31:40 -0300

    RSVP Form

--- web/modules/custom/rsvplist/src/Form/RSVPForm.php
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -62,7 +62,7 @@ class RSVPForm extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $submitted_email = $form_state->getValue('email');
     $this->messenger()->addMessage(t('The form is working! You entered @entry',
-      ['@entry' => $submitted_email]);
+      ['@entry' => $submitted_email]));
 
   }
 
