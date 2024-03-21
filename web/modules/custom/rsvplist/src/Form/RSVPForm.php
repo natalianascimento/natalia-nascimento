2223ab61411a65f78b75ee99d9d916fbd3349b2c
Natalia <natalia.nascc@gmail.com>
2024-03-20 23:14:44 -0300

    Form Changes

--- web/modules/custom/rsvplist/src/Form/RSVPForm.php
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * A form to collect an email address for RSVP details
+ * A form to collect an email address for RSVP details.
  */
 
 namespace Drupal\rsvplist\Form;
@@ -19,7 +19,7 @@ class RSVPForm extends FormBase {
     return 'rsvplist_email_form';
   }
 
-  /*
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
@@ -28,9 +28,10 @@ class RSVPForm extends FormBase {
 
     // Some pages may not be nodes though and $node will be NULL on those pages.
     // If a node was loaded, get the node id.
-    if (!(is_null($node))) {
+    if ( !(is_null($node)) ) {
       $nid = $node->id();
-    } else {
+    }
+    else {
       // If a node could not be loaded, default to 0;
       $nid = 0;
     }
@@ -38,16 +39,18 @@ class RSVPForm extends FormBase {
     // Establish the $form render array. It has an email text field,
     // a submit button, and a hidden field containing the node nid.
     $form['email'] = [
-      '#title' => t('Email address'),
       '#type' => 'textfield',
+      '#title' => t('Email address'),
       '#size' => 25,
       '#description' => t('We will send updates to the email address you provide.'),
       '#required' => TRUE,
     ];
+
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => t('RSVP'),
     ];
+
     $form['nid'] = [
       '#type' => 'hidden',
       '#value' => $nid,
@@ -59,11 +62,21 @@ class RSVPForm extends FormBase {
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $value = $form_state->getValue('email');
+    if ( !(\Drupal::service('email.validator')->isValid($value)) ) {
+      $form_state->setErrorByName('email',
+        $this->t('It appears that %mail is not a valid email. Please try again',
+        ['%mail' => $value]));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $submitted_email = $form_state->getValue('email');
     $this->messenger()->addMessage(t('The form is working! You entered @entry',
       ['@entry' => $submitted_email]));
-
   }
-
 }
