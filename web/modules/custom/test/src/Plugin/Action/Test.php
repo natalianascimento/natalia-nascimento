481c3f9002d97e36045d3d99540b8facbc648c53
Natalia <natalia.nascc@gmail.com>
2024-03-21 23:13:18 -0300

    New test module and fixing routing.yml

---
+++ web/modules/custom/test/src/Plugin/Action/Test.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\test\Plugin\Action;
+
+use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Provides a test action.
+ *
+ * @Action(
+ *   id = "test_test",
+ *   label = @Translation("test"),
+ *   type = "node",
+ *   category = @Translation("Custom"),
+ * )
+ *
+ * @DCG
+ * For updating entity fields consider extending FieldUpdateActionBase.
+ * @see \Drupal\Core\Field\FieldUpdateActionBase
+ *
+ * @DCG
+ * In order to set up the action through admin interface the plugin has to be
+ * configurable.
+ * @see https://www.drupal.org/project/drupal/issues/2815301
+ * @see https://www.drupal.org/project/drupal/issues/2815297
+ *
+ * @DCG
+ * The whole action API is subject of change.
+ * @see https://www.drupal.org/project/drupal/issues/2011038
+ */
+final class Test extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($entity, AccountInterface $account = NULL, $return_as_object = FALSE): AccessResultInterface|bool {
+    /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+    $access = $entity->access('update', $account, TRUE)
+      ->andIf($entity->get('field_example')->access('edit', $account, TRUE));
+    return $return_as_object ? $access : $access->isAllowed();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute(ContentEntityInterface $entity = NULL): void {
+    $entity->set('field_example', 'New value')->save();
+  }
+
+}
